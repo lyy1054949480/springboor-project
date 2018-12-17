618c3d82cb777c36ddff4139ffcf27ff06252d84
liyangyang <1054949480@qq.com>
2018-12-17 20:52:48 +0800

    "first commit"

--- src/main/java/com/baizhi/springboot/SpringbootApplication.java
+++ src/main/java/com/baizhi/springboot/SpringbootApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootApplication {
     public static void main(String[] args) {
+        System.out.println("haha");
         SpringApplication.run(SpringbootApplication.class, args);
     }
 
