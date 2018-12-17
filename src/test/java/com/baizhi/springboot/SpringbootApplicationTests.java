9f0cb6ef5c6ae7fcd02f3baab8b79524f6895131
liyangyang <1054949480@qq.com>
2018-12-17 21:13:41 +0800

    "first commit"

--- src/test/java/com/baizhi/springboot/SpringbootApplicationTests.java
+++ src/test/java/com/baizhi/springboot/SpringbootApplicationTests.java
@@ -1,16 +1,21 @@
 package com.baizhi.springboot;
 
+import com.baizhi.springboot.mapper.UserMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringbootApplicationTests {
-
+    @Autowired
+    UserMapper userMapper
+            ;
     @Test
     public void contextLoads() {
+
     }
 
 }
